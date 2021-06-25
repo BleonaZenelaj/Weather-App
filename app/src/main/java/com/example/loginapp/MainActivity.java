4651767eb774fddc131d39809d8fa4e91da4d9c8
EldinVeliji <44035030+EldinVeliji@users.noreply.github.com>
2021-06-25 15:25:33 +0200

    Update MainActivity.java

--- app/src/main/java/com/example/loginapp/MainActivity.java
+++ app/src/main/java/com/example/loginapp/MainActivity.java
@@ -15,5 +15,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        imageView = findViewById(R.id.imageView);
+        editText = findViewById(R.id.EditText);
+        button = findViewById(R.id.button);
+        country_tx = findViewById(R.id.country);
+        city_tx = findViewById(R.id.city);
+        temp_tx = findViewById(R.id.temperature);
+        latitude_tx = findViewById(R.id.latitude);
+        longitude_tx = findViewById(R.id.longitude);
+        sunrise_tx = findViewById(R.id.sunrise);
+        sunset_tx = findViewById(R.id.sunset);
+        humidity_tx = findViewById(R.id.humidity);
+        pressure_tx = findViewById(R.id.pressure);
+        windSpeed_tx = findViewById(R.id.windSpeed);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                findWeather();
+            }
+        });
     }
 }
